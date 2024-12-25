27ec8e7ed2af9086d66d463f1f2dfd235ad20d00
Ali Dekheel <adekheel@gmail.com>
2024-12-25 16:29:49 +0200

    remove single child scroll view

--- lib/Tabs/Sebha/sebha_tab.dart
+++ lib/Tabs/Sebha/sebha_tab.dart
@@ -21,71 +21,68 @@ class _SebhaTabState extends State<SebhaTab> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      physics: null,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(
-            AppAssets.logo,
-            height: screenSize.height * .20,
-          ),
-          Center(
-            child: Text("سَبِّحِ اسْمَ رَبِّكَ الأعلى ",
-                style: AppTheme.darkTheme.textTheme.headlineLarge
-                    ?.copyWith(fontSize: 26)),
-          ),
-          Center(
-            child: Stack(alignment: Alignment.center, children: [
-              Positioned(
-                top: -screenSize.height * .36,
-                left: screenSize.width * .15,
-                right: 0,
-                bottom: 0,
-                child: Image.asset(
-                  AppAssets.sebhaHead,
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          AppAssets.logo,
+          height: screenSize.height * .20,
+        ),
+        Center(
+          child: Text("سَبِّحِ اسْمَ رَبِّكَ الأعلى ",
+              style: AppTheme.darkTheme.textTheme.headlineLarge
+                  ?.copyWith(fontSize: 26)),
+        ),
+        Center(
+          child: Stack(alignment: Alignment.center, children: [
+            Positioned(
+              top: -screenSize.height * .36,
+              left: screenSize.width * .15,
+              right: 0,
+              bottom: 0,
+              child: Image.asset(
+                AppAssets.sebhaHead,
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 90),
-                child: InkWell(
-                  onTap: _handleTasbeeh,
-                  child: Transform.rotate(
-                    angle: degrees,
-                    child: Image.asset(
-                      AppAssets.sebhaBody,
-                      height: screenSize.height * .40,
-                      fit: BoxFit.fill,
-                    ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 90),
+              child: InkWell(
+                onTap: _handleTasbeeh,
+                child: Transform.rotate(
+                  angle: degrees,
+                  child: Image.asset(
+                    AppAssets.sebhaBody,
+                    height: screenSize.height * .40,
+                    fit: BoxFit.fill,
                   ),
                 ),
               ),
-              Positioned(
-                top: screenSize.height * .27,
-                child: Column(
-                  children: [
-                    Text(tasbeehWords[index],
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineLarge
-                            ?.copyWith(fontSize: 24)),
-                    SizedBox(
-                      height: screenSize.height * .07,
-                    ),
-                    Text(
-                      "$counter",
+            ),
+            Positioned(
+              top: screenSize.height * .27,
+              child: Column(
+                children: [
+                  Text(tasbeehWords[index],
                       style: Theme.of(context)
                           .textTheme
                           .headlineLarge
-                          ?.copyWith(fontSize: 24),
-                    ),
-                  ],
-                ),
-              )
-            ]),
-          ),
-        ],
-      ),
+                          ?.copyWith(fontSize: 24)),
+                  SizedBox(
+                    height: screenSize.height * .07,
+                  ),
+                  Text(
+                    "$counter",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineLarge
+                        ?.copyWith(fontSize: 24),
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
+      ],
     );
   }
 
